5d3952c3c1598b9d7f7121a80fa95f01c908e271
unknown <erzikizineb4@gmail.com>
2025-05-06 09:15:40 +0100

    exercice

---
+++ week01/day01/exercices/ex1.py
@@ -0,0 +1,44 @@
+# exercice01 :
+print("Hello world\nHello world\nHello world\nHello world")
+
+# exercice02 :
+print((99**3) * 8)
+
+# exercice03  :
+name = input('What is your name? ') 
+if name == "simon":
+    print("Hey Simon! You're a legend!")
+elif name == "zineb":
+    print("Zineb, such a powerful name!")
+else:
+    print("Welcome, glad to have you here!")
+
+# exercice04  :
+height = int(input('what is your height in centimeters?'))
+if height >= 145:
+    print("your height is enough for the ride")
+else :
+    print("oh no,you might need to grow  a little more to be able to go on the ride.")
+
+# exercice05  :
+my_fav_numbers = {4, 8, 17}
+my_fav_numbers.add(6)
+my_fav_numbers.add(12)
+my_fav_numbers.remove(12)
+
+friend_fav_numbers = {1, 5, 9}
+
+our_fav_numbers = my_fav_numbers.copy()
+our_fav_numbers.update(friend_fav_numbers)
+
+print("My favorite numbers:", my_fav_numbers)
+print("Friend's favorite numbers:", friend_fav_numbers)
+print("Our favorite numbers:", our_fav_numbers)
+
+# DAILY CHALLENGES :
+
+
+
+
+    
+    
