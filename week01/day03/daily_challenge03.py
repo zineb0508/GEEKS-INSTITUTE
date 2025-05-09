2ddbc4c8a3ac7197c3fdce466b3bf5b57a0cc27e
unknown <erzikizineb4@gmail.com>
2025-05-09 21:31:57 +0100

    .\daily_challenge03.py

--- week01/day03/daily_challenge03.py
+++ week01/day03/daily_challenge03.py
@@ -1,5 +1,5 @@
   ## CHALLENGE :
-
+ 
 
 
 class Farm:
@@ -20,26 +20,8 @@ class Farm:
         info += "\n    E-I-E-I-0!"
         return info
 
-    def get_animal_types(self):
-        return sorted(self.animals.keys())
 
-    def get_short_info(self):
-        animal_types = self.get_animal_types()
-        animal_list = []
-        for animal in animal_types:
-            count = self.animals[animal]
-            if count > 1:
-                animal_list.append(animal + 's')
-            else:
-                animal_list.append(animal)
-        if len(animal_list) == 1:
-            animals_str = animal_list[0]
-        else:
-            animals_str = ', '.join(animal_list[:-1]) + f" and {animal_list[-1]}"
-        return f"{self.name}'s farm has {animals_str}."
-
-
-
+#
 if __name__ == "__main__":
     macdonald = Farm("McDonald")
     macdonald.add_animal('cow', 5)
@@ -48,7 +30,3 @@ if __name__ == "__main__":
     macdonald.add_animal('goat', 12)
 
     print(macdonald.get_info())
-    print()  # just for spacing
-    print(macdonald.get_animal_types())
-    print(macdonald.get_short_info())
-
