b468261eb53b09cdc76396d7d365e69266721d4e
unknown <erzikizineb4@gmail.com>
2025-05-13 21:33:32 +0100

    exerciceXP.sql

---
+++ week02/day001/day002/exerciceXP.sql
@@ -0,0 +1,5 @@
+SELECT * FROM items ;
+SELECT price FROM items ORDER BY price DESC;
+SELECT price FROM items WHERE price >= 80 ORDER BY price ASC;
+SELECT firstname , lastname FROM customers ORDER BY firstname ASC LIMIT 3;
+SELECT lastname FROM customers ORDER BY lastname DESC ;
